0099e6736ca0520cb52f7b5521e817c990533f3b
dxcezza <48048129+dxcezza@users.noreply.github.com>
2025-03-29 09:59:44 +0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -19,13 +19,15 @@ def download_track(url):
     try:
         logging.info(f"Начинаем скачивание трека по URL: {url}")
         
-        # Получаем настройки SOCKS5-прокси из переменных окружения
-        socks_proxy = os.getenv("SOCKS5_PROXY")
-        if not socks_proxy:
-            logging.warning("SOCKS5-прокси не настроен. Используется прямое соединение.")
+        # Получаем настройки прокси из переменных окружения
+        http_proxy = os.getenv("HTTP_PROXY")
+        https_proxy = os.getenv("HTTPS_PROXY")
         
-        # Формируем команду spotdl
-        proxy_args = f"--proxy {socks_proxy}" if socks_proxy else ""
+        if not http_proxy or not https_proxy:
+            logging.warning("Прокси не настроен. Используется прямое соединение.")
+        
+        # Формируем команду spotdl с учетом прокси
+        proxy_args = f"--proxy {http_proxy}" if http_proxy else ""
         command = f"spotdl {url} {proxy_args}"
         logging.debug(f"Выполняем команду: {command}")
         
@@ -35,9 +37,10 @@ def download_track(url):
         
         # Запускаем процесс с перехватом stdout и stderr
         env = os.environ.copy()
-        if socks_proxy:
-            env["HTTP_PROXY"] = socks_proxy
-            env["HTTPS_PROXY"] = socks_proxy
+        if http_proxy:
+            env["HTTP_PROXY"] = http_proxy
+        if https_proxy:
+            env["HTTPS_PROXY"] = https_proxy
         
         with subprocess.Popen(command, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env) as process:
             output = []
